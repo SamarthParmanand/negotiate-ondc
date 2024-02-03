ea571fd4ed41294ab211b15e61653676b4df4214
Yogesh Dubey <88256165+Yogesh-Dubey-Ayesavi@users.noreply.github.com>
2024-02-03 23:39:06 +0530

    added session repo

---
+++ src/app/(chat)/repositories/chat_session_repository.ts
@@ -0,0 +1,35 @@
+import supabaseClient from "@/utils/supabaseClient";
+import { SupabaseClient } from "@supabase/supabase-js";
+import ChatSession from "../models/chat_session";
+
+abstract class ChatSessionRepository{
+    abstract fetchSessions():Promise<ChatSession[]>;
+    abstract createSession(session:ChatSession):Promise<void>;    
+}
+
+/**
+ * Example usage
+ ```typescript
+    var _repo =  new SessionRepository();
+ ```
+ */
+export class SessionRepository implements ChatSessionRepository{
+    private supabase :SupabaseClient;
+
+    constructor(){
+        this.supabase = supabaseClient();
+    }
+
+ async fetchSessions(): Promise<ChatSession[]> {
+    const {data,error} = await this.supabase.from("chat_sessions").select("*");
+    if(data){
+        return data.map((rawSession)=>new ChatSession(rawSession));
+    }
+    throw error;
+    }
+
+ async createSession(session:ChatSession): Promise<void> {
+        await this.supabase.from("chat_sessions").insert(session);
+    }
+
+}
