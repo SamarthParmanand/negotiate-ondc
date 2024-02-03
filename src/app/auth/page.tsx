1ad9330f6425070ca7f383279d3893f93620bca3
SamarthParmanand <parmanandsamarth@gmail.com>
2024-02-03 13:20:49 +0530

    fix: oauth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -6,7 +6,7 @@ import supabaseClient from "@/utils/supabaseClient";
 export default function Page() {
   const supabase = supabaseClient();
   const handleLogin = () => {
-    const { data, error } = supabase.auth.signInWithOAuth({
+    const { error } = supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
         redirectTo: location.origin + "/auth/callback",
